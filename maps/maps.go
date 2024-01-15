9c6a0e44c99374b658fd2a69384a8c9f8c34e4ec
Yoolayn <jaroszynskijulian@gmail.com>
2024-01-15 10:19:10 +0100

    feat: maps

--- maps/maps.go
+++ maps/maps.go
@@ -1,17 +1,68 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
 
-var (
-	 NotFoundErr = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound          = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists        = DictionaryErr("definition already exists!")
+	ErrWordDoesNotExists = DictionaryErr("definition not updated, word not found")
+	ErrDelete            = DictionaryErr("word not deleted; it doesn't exist already!")
 )
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", NotFoundErr
+		return "", ErrNotFound
 	}
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = definition
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		delete(d, word)
+	case ErrNotFound:
+		return ErrDelete
+	default:
+		return err
+	}
+	return nil
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -10,6 +10,23 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+	assertStrings(t, got, definition)
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 	t.Run("known word", func(t *testing.T) {
@@ -20,12 +37,76 @@ func TestSearch(t *testing.T) {
 	})
 	t.Run("unknown word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
-		want := NotFoundErr
+		want := ErrNotFound
 
 		if err == nil {
 			t.Fatal("expected an error")
 		}
 
 		assertStrings(t, err.Error(), want.Error())
+	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		d := Dictionary{}
+		_ = d.Add("test", "this is just a test")
+
+		want := "this is just a test"
+
+		assertDefinition(t, d, "test", want)
+	})
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+		newDefintion := "new definition"
+
+		_ = dictionary.Update(word, newDefintion)
+
+		assertDefinition(t, dictionary, word, newDefintion)
+	})
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{}
+		newDefintion := "new definition"
+
+		err := dictionary.Update(word, newDefintion)
+
+		assertError(t, err, ErrWordDoesNotExists)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{word: definition}
+
+		_ = dictionary.Delete(word)
+
+		_, err := dictionary.Search(word)
+		if err != ErrNotFound {
+			t.Errorf("expected %q to be deleted", word)
+		}
+	})
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Delete(word)
+		assertError(t, err, ErrDelete)
 	})
 }
